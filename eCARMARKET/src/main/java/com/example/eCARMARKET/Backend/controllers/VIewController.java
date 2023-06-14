7c5639871627784a3b56d77d7de14274955bdc82
macontrerascordoba <macontrerascordoba@gmail.com>
2023-06-14 10:05:00 +0200

    Simple front to test Login

---
+++ eCARMARKET/src/main/java/com/example/eCARMARKET/Backend/controllers/VIewController.java
@@ -0,0 +1,18 @@
+package com.example.eCARMARKET.Backend.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class VIewController {
+    
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+}
