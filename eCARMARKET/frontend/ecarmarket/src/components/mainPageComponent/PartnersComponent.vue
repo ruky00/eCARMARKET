5c327ee7fea1fc59fc2b90e2ad874b330c20dc72
ruky00 <c.fernandezl.2019@alumnos.urjc.es>
2023-06-19 09:42:04 +0200

    partners animations

--- eCARMARKET/frontend/ecarmarket/src/components/mainPageComponent/PartnersComponent.vue
+++ eCARMARKET/frontend/ecarmarket/src/components/mainPageComponent/PartnersComponent.vue
@@ -1,12 +1,48 @@
 <template>
     <div id="partners-section">
         <h2>PARTNER COMPANIES</h2>
-        <div class="images">
-            <img src="../../assets/images/tesla.png" alt="Tesla Logo">
-            <img src="../../assets/images/byd1.png" alt="BYD Logo" style="height: 200px;">
-            <img src="../../assets/images/cupra.png" alt="Cupra Logo" style="height: 300px;">
-            
-        </div>
+        <div class="boxesContainer">
+
+<div class="cardBox">
+  <div class="card">
+    <div class="front">
+        <img src="../../assets/images/tesla.png" alt="Tesla Logo" style="width:300px; height: 285px;">
+    </div>
+    <div class="back">
+      <h3>Back Side One</h3>
+      <p>Content in card one</p>
+      <a href="#">Button 1</a>
+    </div>
+  </div>
+</div>
+
+<div class="cardBox">
+  <div class="card">
+    <div class="front">
+        <img src="../../assets/images/byd1.png" alt="BYD Logo" style="height: 200px; margin-top: 50px;">
+    </div>
+    <div class="back">
+      <h3>Back Side Two</h3>
+      <p>Content in card two</p>
+      <a href="#">Button 2</a>
+    </div>
+  </div>
+</div>
+
+<div class="cardBox">
+  <div class="card">
+    <div class="front">
+        <img src="../../assets/images/cupra.png" alt="Cupra Logo" style="height: 200px; width: 250px; margin-top: 40px;">
+    </div>
+    <div class="back">
+      <h3>Back Side Three</h3>
+      <p>Content in card three</p>
+      <a href="#">Button 3</a>
+    </div>
+  </div>
+</div>
+
+    </div>
     </div>
 </template>
 
