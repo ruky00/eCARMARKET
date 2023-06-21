3c9b2ea11a6e1a75e62bcf5518d62d247850a0b8
macontrerascordoba <macontrerascordoba@gmail.com>
2023-06-21 13:56:01 +0200

    Stock section modification partially done

--- eCARMARKET/frontend/ecarmarket/src/components/mainPageComponent/StocksComponent.vue
+++ eCARMARKET/frontend/ecarmarket/src/components/mainPageComponent/StocksComponent.vue
@@ -2,9 +2,9 @@
     <div id="second-section">
         <h2>THE STOCK MARKET WITHIN YOUR REACH</h2>
         <div class="stocks">
-            <img src="@/assets/images/stock.png" alt="" id="s1" class="not-focused" @click="changeState('s1')">
-            <img src="@/assets/images/stock.png" alt="" id="s2" class="focused" @click="changeState('s2')">
-            <img src="@/assets/images/stock.png" alt="" id="s3" class="not-focused" @click="changeState('s3')">
+            <img src="@/assets/images/stock1.png" alt="" id="s1" class="not-focused" @click="changeState('s1')">
+            <img src="@/assets/images/stock2.png" alt="" id="s2" class="focused" @click="changeState('s2')">
+            <img src="@/assets/images/stock3.png" alt="" id="s3" class="not-focused" @click="changeState('s3')">
         </div>
     </div>
 
@@ -17,11 +17,10 @@
         name: 'StocksComponent',
         methods: {
             changeState(clickedId) {
-                var focusedId = document.getElementsByClassName("focused")[0].id;
-                document.getElementById(focusedId).classList.remove("focused");
-                document.getElementById(focusedId).classList.add("not-focused");
-                document.getElementById(clickedId).classList.remove("not-focused");
-                document.getElementById(clickedId).classList.add("focused");
+                var clickedImg = document.getElementById(clickedId).src;
+                var centerImg = document.getElementById('s2').src;
+                document.getElementById(clickedId).src = centerImg;
+                document.getElementById('s2').src = clickedImg;
             }
         }
     }
