e25548702b6a6594d4e1ff97c4ecb016a3f84a4d
ruky00 <c.fernandezl.2019@alumnos.urjc.es>
2023-06-22 09:22:01 +0200

    Login Connections

--- eCARMARKET/frontend/ecarmarket/src/components/authComponents/LoginComponent.vue
+++ eCARMARKET/frontend/ecarmarket/src/components/authComponents/LoginComponent.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="cont">
     <div class="form sign-in">
+      <form @submit.prevent="sesionStart" name="id">
       <h2>Sign In</h2>
       <label>
         <span>Email Address</span>
-        <input type="email" name="email">
+        <input type="email" v-model="email">
       </label>
       <label>
         <span>Password</span>
-        <input type="password" name="password">
+        <input type="password" v-model="password">
       </label>
-      <button class="submit" type="button">Sign In</button>
+      <button class="submit" type="submit">Sign In</button>
+      </form>
       <p class="forgot-pass">Forgot Password ?</p>
     <router-link to="/" style="text-decoration: none;"><p class="forgot-pass">Go back home</p></router-link> 
     </div>
@@ -48,7 +50,7 @@
           <span>Confirm Password</span>
           <input type="password">
         </label>
-        <button type="button" class="submit">Sign Up Now</button>
+        <button type="submit">Sign Up Now</button>
       </div>
     </div>
   </div>
@@ -57,11 +59,31 @@
   <script>
     export default{
         name:'LoginComponent',
+        data(){
+          return{
+            email:"",
+            password:""
+          }
+        },
         methods: {
     // Métodos del componente
-    toggleSignup() {
-      document.querySelector('.cont').classList.toggle('s-signup');
-    },
+      async sesionStart(){
+        var payload = {
+          email:this.email,
+          password:this.password
+        };
+        await this.axios.post('/login', payload)
+        .then(response => {console.log(response);
+        })
+        .cath(error =>{
+          var data = error.response.data;
+          console.log(data)
+        })
+      },  
+      toggleSignup() {
+        document.querySelector('.cont').classList.toggle('s-signup');
+      },
+    
   },
   mounted() {
     // Lógica adicional al montar el componente
--- eCARMARKET/frontend/ecarmarket/src/main.js
+++ eCARMARKET/frontend/ecarmarket/src/main.js
@@ -3,6 +3,8 @@ import 'mutationobserver-shim'
 import { createApp } from 'vue'
 import App from './App.vue'
 import routes from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+axios.defaults.baseURL = 'http://localhost:8080/api'
 
-
-createApp(App).use(routes).mount('#app')
+createApp(App).use(routes).use(VueAxios,axios).mount('#app')
