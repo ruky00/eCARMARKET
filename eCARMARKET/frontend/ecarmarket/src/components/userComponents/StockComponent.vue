d8d5dc17e701bedc48982758aa9c1d53648a7dd2
macontrerascordoba <macontrerascordoba@gmail.com>
2023-07-12 10:48:45 +0200

    Stock colors changed

--- eCARMARKET/frontend/ecarmarket/src/components/userComponents/StockComponent.vue
+++ eCARMARKET/frontend/ecarmarket/src/components/userComponents/StockComponent.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="stock">
-        <div class="buttons">
-            <button type="button" @click="generateGraph(this.graphId, '1')">1</button>
-            <button type="button" @click="generateGraph(this.graphId, '2')">2</button>
-            <button type="button" @click="generateGraph(this.graphId, '3')">3</button>
+        <div class="buttons" id="buttons">
+            <button type="button" class="selected" id='1' @click="moveGraph('1')">1</button>
+            <button type="button" id='2' @click="moveGraph('2')">2</button>
+            <button type="button" id='3' @click="moveGraph('3')">3</button>
         </div>
         <div class="graph" :id="graphId">
         </div>
@@ -32,6 +32,7 @@
                 y: '',
                 svg: '',
                 height: '',
+                currentId: '',
             }    
         },
         mounted() {
@@ -39,14 +40,15 @@
             script.src = 'https://d3js.org/d3.v7.min.js';
             script.async = true;
             script.onload = () => {
-                this.generateGraph(this.graphId);
+                this.generateGraph();
             };
             document.head.appendChild(script);
         },
         methods: {
-            async generateGraph(graphId, id='1') {
-                
-                document.getElementById(`${graphId}`).innerHTML = '';
+            generateGraph(id='1', pos='moveMid') {
+                this.currentId = id;
+                document.getElementById(`${this.graphId}`).innerHTML = '';
+
 
                 // Set dimensions and margins for the chart
 
@@ -65,26 +67,20 @@
 
                 // Create the SVG element and append it to the chart container
 
-                var pos = ''
-
-                if (id === '1') pos = 'moveLeft';
-                else if (id === '2') pos = 'moveMid';
-                else if (id === '3') pos = 'moveRight'
-
                 var heading = document.createElement('h2');
                 heading.id = this.graphName;
                 heading.classList.add(pos);
-                document.getElementById(graphId).appendChild(heading);
+                document.getElementById(this.graphId).appendChild(heading);
 
-                this.svg = window.d3.select(`#${graphId}`)
+                this.svg = window.d3.select(`#${this.graphId}`)
                     .append("svg")
                         .attr("viewBox", "0 0 900 400")
-                        .attr("id", `${graphId}Svg`)
+                        .attr("id", `${this.graphId}Svg`)
                         .attr("class", pos)
                     .append("g")
                         .attr("transform", `translate(${margin.left},${margin.top})`);
 
-                if (graphId === "graphRight") {
+                if (this.graphId === "graphRight") {
                     window.d3.select("#graphRightSvg").attr("height", "15vw")
                 }
                 
@@ -127,6 +123,65 @@
                     this.x.domain(window.d3.extent(data, d => d.date));
                     this.y.domain([window.d3.min(data, d => d.value) - 3, window.d3.max(data, d => d.value)]);
 
+                    // Create the line segments
+                    const lineSegments = [];
+                    let segmentData = [];
+                    let isGoingUp = false;
+                    let isGoingDown = false;
+
+                    // Loop through the data to create line segments
+                    for (let i = 0; i < data.length; i++) {
+                        const currentData = data[i];
+                        const previousData = i > 0 ? data[i - 1] : null;
+
+                        // Check if the line is going up or down
+                        if (previousData && currentData.value > previousData.value) {
+                            if (isGoingDown) {
+                                // If the line was going down, start a new segment
+                                lineSegments.push(segmentData);
+                                segmentData = [previousData];
+                            }
+                            isGoingUp = true;
+                            isGoingDown = false;
+                        } else if (previousData && currentData.value < previousData.value) {
+                            if (isGoingUp) {
+                                // If the line was going up, start a new segment
+                                lineSegments.push(segmentData);
+                                segmentData = [previousData];
+                            }
+                            isGoingUp = false;
+                            isGoingDown = true;
+                        }
+
+                        // Add the current data to the segment
+                        segmentData.push(currentData);
+
+                        // If it's the last data point, add the final segment
+                        if (i === data.length - 1) {
+                            lineSegments.push(segmentData);
+                        }
+                    }
+
+                    // Create the line generator
+                    const self = this;
+                    const line = window.d3.line()
+                        .x(d => self.x(d.date))
+                        .y(d => self.y(d.value));
+
+                    // Add the line segments to the SVG element
+                    lineSegments.forEach((segment) => {
+                        const isGoingUp = segment[segment.length - 1].value > segment[0].value;
+                        const segmentColor = isGoingUp ? 'red' : 'green';
+
+                        this.svg
+                            .append('path')
+                            .datum(segment)
+                            .attr('fill', 'none')
+                            .attr('stroke', segmentColor)
+                            .attr('stroke-width', 3)
+                            .attr('d', line);
+                    });
+
                     // Add the x-axis
 
                     this.svg.append("g")
@@ -141,103 +196,60 @@
                         .call(window.d3.axisLeft(this.y))
                         .attr("stroke-width", 4)
                         .attr("class", "yAxis");
-
-                    // Create the line generator
-                    const self = this;
-                    const line = window.d3.line()
-                        .x(d => self.x(d.date))
-                        .y(d => self.y(d.value));
-
-                    this.svg.selectAll(".xAxis")
-                        .transition()
-                        .duration(3000)
-                        .call(window.d3.axisBottom(this.x))
-                        
-                    this.svg.selectAll(".yAxis")
-                        .transition()
-                        .duration(3000)
-                        .call(window.d3.axisLeft(this.y))
-
-                    // Add the line path to the SVG element
-
-                    this.svg.append("path")
-                        .datum(data)
-                        .attr("fill", "none")
-                        .attr("stroke", "steelblue")
-                        .attr("stroke-width", 5)
-                        .attr("d", line)
                 });
-
-                /* // Get the current date
-                var today = new Date();
-
-                // Substract 1 day from the current date
-                today.setDate(today.getDate() - 1);
-
-                // Get the year, month and day components
-                const year = today.getFullYear();
-                const month = (today.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
-                const day = today.getDate().toString().padStart(2, '0');
-
-                // Format the date string in YYYY-MM-DD format
-
-                const yesterday = `${year}-${month}-${day}`
-
-                console.log(yesterday)
-
-                // Create a dataset
-
-                var url = `/s1.json`//'http://localhost:8081/api/monthly?symbol=' + graphId;
-
-                window.$.getJSON(url, function(jsonData) {
-                    const dataset = [];
-
-                    Object.keys(jsonData["Time Series (Daily)"]).forEach(key => {
-                        dataset.push({ date: new Date(key), value: jsonData["Time Series (Daily)"][key]["4. close"]})
-                    });
-                    document.getElementById(`${graphId}Name`).innerHTML = jsonData["Meta Data"]["2. Symbol"]; */
-
-                    /* Object.keys(jsonData["Monthly Time Series"]).forEach(key => {
-                        dataset.push({ date: new Date(key), value: jsonData["Monthly Time Series"][key]["4. close"]})
-                    });
-                    document.getElementById(`${graphId}Name`).innerHTML = jsonData["Meta Data"]["2. Symbol"]; */
-
-                    // Define the x and y domains
-
-                    /* x.domain(window.d3.extent(dataset, d => d.date));
-                    y.domain([window.d3.min(dataset, d => d.value) - 3, window.d3.max(dataset, d => d.value)]);
-
-                    // Add the x-axis
-
-                    svg.append("g")
-                        .attr("transform", `translate(0,${height})`)
-                        .call(window.d3.axisBottom(x))
-                        .attr("stroke-width", 4)
-                        .attr("class", "xAxis");
-
-                    // Add the y-axis
-
-                    svg.append("g")
-                        .call(window.d3.axisLeft(y))
-                        .attr("stroke-width", 4)
-                        .attr("class", "yAxis");
-
-                    // Create the line generator
-
-                    const line = window.d3.line()
-                        .x(d => x(d.date))
-                        .y(d => y(d.value));
-
-                    // Add the line path to the SVG element
-
-                    svg.append("path")
-                        .datum(dataset)
-                        .attr("fill", "none")
-                        .attr("stroke", "steelblue")
-                        .attr("stroke-width", 5)
-                        .attr("d", line)
-                }); */
             },
+
+            moveGraph(id) {
+
+                if (this.currentId < id) {
+
+                    const currentSvgOld = document.getElementById(`${this.graphId}Svg`);
+                    currentSvgOld.classList.remove('moveMid');
+                    currentSvgOld.classList.add('moveLeft');
+
+                    const currentH2Old = document.getElementById(this.graphName);
+                    currentH2Old.classList.remove('moveMid');
+                    currentH2Old.classList.add('moveLeft');
+
+                    setTimeout(() => {
+                        this.generateGraph(id, 'moveRight');
+                        setTimeout(() => {
+                            const currentSvgOld = document.getElementById(`${this.graphId}Svg`);
+                            currentSvgOld.classList.remove('moveRight');
+                            currentSvgOld.classList.add('moveMid');
+
+                            const currentH2Old = document.getElementById(this.graphName);
+                            currentH2Old.classList.remove('moveRight');
+                            currentH2Old.classList.add('moveMid');
+                        }, 5);
+                    }, 500);
+                }
+                else if (this.currentId > id) {
+                    const currentSvgOld = document.getElementById(`${this.graphId}Svg`);
+                    currentSvgOld.classList.remove('moveMid');
+                    currentSvgOld.classList.add('moveRight');
+
+                    const currentH2Old = document.getElementById(this.graphName);
+                    currentH2Old.classList.remove('moveMid');
+                    currentH2Old.classList.add('moveRight');
+
+                    setTimeout(() => {
+                        this.generateGraph(id, 'moveLeft');
+                        setTimeout(() => {
+                            const currentSvgOld = document.getElementById(`${this.graphId}Svg`);
+                            currentSvgOld.classList.remove('moveLeft');
+                            currentSvgOld.classList.add('moveMid');
+
+                            const currentH2Old = document.getElementById(this.graphName);
+                            currentH2Old.classList.remove('moveLeft');
+                            currentH2Old.classList.add('moveMid');
+                        }, 10);
+                    }, 500);
+                }
+
+                document.getElementById(this.currentId).classList.remove('selected');
+                document.getElementById(id).classList.add('selected');
+            }
         }
     }
 </script>
