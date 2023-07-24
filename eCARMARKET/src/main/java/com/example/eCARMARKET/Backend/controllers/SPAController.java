bb93e86290e45522c7429c8a3392c42dcb050dd3
ruky00 <c.fernandezl.2019@alumnos.urjc.es>
2023-07-24 08:42:06 +0200

    Docker image for frontend not functional

---
+++ eCARMARKET/src/main/java/com/example/eCARMARKET/Backend/controllers/SPAController.java
@@ -0,0 +1,13 @@
+package com.example.eCARMARKET.Backend.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SPAController {
+
+    @GetMapping({"/new/**/{path:[^\\.]*}", "/{path:new[^\\.]*}"})
+    public String redirect() {
+        return "forward:/new/index.html";
+    }
+}
